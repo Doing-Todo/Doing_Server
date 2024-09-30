9d5fdc9f071dfbd9d9a708629fc0d46dbd7d094f
hanni66 <haeunkim0807@gmail.com>
2024-09-30 14:22:42 +0900

    setting : new project

---
+++ src/test/java/org/skhu/doing/DoingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skhu.doing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
