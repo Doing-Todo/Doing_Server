0d6a525f7691b8b67ab3e0705049f572faa9fecc
hanni66 <72500673+hanni66@users.noreply.github.com>
2024-11-04 13:22:18 +0900

    feat : member controller, repository, service 개발 (#8)

--- src/main/java/org/skhu/doing/entity/Member.java
+++ src/main/java/org/skhu/doing/entity/Member.java
@@ -1,4 +1,34 @@
 package org.skhu.doing.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long kakaoMember;
+
+    @Column(length = 255)
+    private String email;
+
+    @Column(length = 100)
+    private String nickname;
+
+    public Member(Long kakaoMember, String email, String nickname) {
+        this.kakaoMember = kakaoMember;
+        this.email = email;
+        this.nickname = nickname;
+    }
 }
+
--- src/main/java/org/skhu/doing/service/MemberService.java
+++ src/main/java/org/skhu/doing/service/MemberService.java
@@ -1,4 +1,11 @@
 package org.skhu.doing.service;
 
-public class MemberService {
+import org.skhu.doing.entity.Member;
+
+import java.util.Optional;
+
+public interface MemberService {
+    Member findOrCreateMember(Long kakaoMemberId, String email, String nickname);
+    Optional<Member> findByKakaoMember(Long kakaoMemberId);
+    Member updateMember(Long memberId, String nickname);
 }
