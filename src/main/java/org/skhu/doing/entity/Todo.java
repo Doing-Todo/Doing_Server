badd1d08ca1b09497d87a1409ca4b05c8a029c7d
hanni66 <72500673+hanni66@users.noreply.github.com>
2024-11-04 12:18:51 +0900

    feat : todo controller, repository, service 개발 (#4)

--- src/main/java/org/skhu/doing/entity/Todo.java
+++ src/main/java/org/skhu/doing/entity/Todo.java
@@ -1,4 +1,46 @@
 package org.skhu.doing.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Todo")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(name = "folder_id", nullable = false)
+    private Long folderId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public enum Status {
+        PENDING, IN_PROGRESS, COMPLETED
+    }
 }
