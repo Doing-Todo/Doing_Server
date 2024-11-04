e3321ec8882c08e464a77f7a50f125f402563106
hanni66 <haeunkim0807@gmail.com>
2024-11-04 12:16:41 +0900

    feat : todo controller, repository, service 개발

--- src/main/java/org/skhu/doing/repository/TodoRepository.java
+++ src/main/java/org/skhu/doing/repository/TodoRepository.java
@@ -1,4 +1,14 @@
 package org.skhu.doing.repository;
 
-public class TodoRepository {
+import org.skhu.doing.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    // 상태로 Todo 검색
+    List<Todo> findByStatus(String status);
+
+    // 설명에 특정 키워드가 포함된 Todo 검색
+    List<Todo> findByDescriptionContaining(String keyword);
+
 }
