65fad828203d930cf2f82b819aee2967495fcadd
hanni66 <haeunkim0807@gmail.com>
2024-11-04 13:20:04 +0900

    feat : member controller, repository, service 개발

--- src/main/java/org/skhu/doing/repository/MemberRepository.java
+++ src/main/java/org/skhu/doing/repository/MemberRepository.java
@@ -1,4 +1,10 @@
 package org.skhu.doing.repository;
 
-public class MemberRepository {
+import org.skhu.doing.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByKakaoMember(Long kakaoMember);
 }
