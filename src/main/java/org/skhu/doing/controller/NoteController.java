a4ce1f3232992d06b82e4bfebddd3107bca9b703
hanni66 <haeunkim0807@gmail.com>
2024-11-04 12:19:34 +0900

    feat : note controller, repository, service 개발

--- src/main/java/org/skhu/doing/controller/NoteController.java
+++ src/main/java/org/skhu/doing/controller/NoteController.java
@@ -1,4 +1,52 @@
 package org.skhu.doing.controller;
 
+import org.skhu.doing.entity.Note;
+import org.skhu.doing.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notes")
 public class NoteController {
+
+    private final NoteService noteService;
+
+    @Autowired
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Note> createNote(@RequestBody Note note) {
+        Note createdNote = noteService.createNote(note);
+        return new ResponseEntity<>(createdNote, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
+        Note note = noteService.getNoteById(id);
+        return new ResponseEntity<>(note, HttpStatus.OK);
+    }
+
+    @GetMapping("/folder/{folderId}")
+    public ResponseEntity<List<Note>> getNotesByFolderId(@PathVariable Long folderId) {
+        List<Note> notes = noteService.getNotesByFolderId(folderId);
+        return new ResponseEntity<>(notes, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Note> updateNote(@PathVariable Long id, @RequestBody Note note) {
+        Note updatedNote = noteService.updateNote(id, note);
+        return new ResponseEntity<>(updatedNote, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
+        noteService.deleteNoteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
