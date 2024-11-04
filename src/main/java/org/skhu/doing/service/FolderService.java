427450bd046ae7142c359a665e6c180c0fd6c5fe
hanni66 <haeunkim0807@gmail.com>
2024-11-04 12:21:23 +0900

    feat : folder controller, repository, service 개발

--- src/main/java/org/skhu/doing/service/FolderService.java
+++ src/main/java/org/skhu/doing/service/FolderService.java
@@ -1,4 +1,13 @@
 package org.skhu.doing.service;
 
-public class FolderService {
+import org.skhu.doing.entity.Folder;
+
+import java.util.List;
+
+public interface FolderService {
+    Folder createFolder(Folder folder);
+    Folder getFolderById(Long id);
+    List<Folder> getFoldersByMemberId(Long memberId);
+    Folder updateFolder(Long id, Folder folder);
+    void deleteFolderById(Long id);
 }
