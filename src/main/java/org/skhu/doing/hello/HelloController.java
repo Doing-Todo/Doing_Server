9ba3b7d549d6751e739076f7f34ab9da16f73993
hanni66 <haeunkim0807@gmail.com>
2024-11-28 01:16:58 +0900

    fix : test controller 수정

--- src/main/java/org/skhu/doing/hello/HelloController.java
+++ src/main/java/org/skhu/doing/hello/HelloController.java
@@ -1,9 +1,8 @@
-package org.skhu.doing;
+package org.skhu.doing.hello;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
